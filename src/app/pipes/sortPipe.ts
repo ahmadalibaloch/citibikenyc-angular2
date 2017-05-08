e46e34cdb158803e9d634f8d0b59537101aeb2a3
Ahmad Ali <ahmadalibaloch@gmail.com>
2017-05-08 09:43:32 +0500

    pipe fix

--- src/app/pipes/sortPipe.ts
+++ src/app/pipes/sortPipe.ts
@@ -1,13 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
 @Pipe({ name: 'sort', pure: false })
 export class SortPipe implements PipeTransform {
     transform(items: any[], field: string, order: string): any[] {
